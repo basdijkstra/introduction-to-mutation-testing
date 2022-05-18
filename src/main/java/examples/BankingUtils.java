25164b9da7d71e05a68ba3d6440b1f300e659c5f
Bas Dijkstra <bas@ontestautomation.com>
2022-05-18 08:43:17 +0200

    Updated pitest version

--- src/main/java/examples/BankingUtils.java
+++
@@ -1,9 +0,0 @@
-package examples;
-
-public class BankingUtils {
-
-    public static int generateAccountNumber() {
-
-        return (int) (Math.random() * (100000 - 10000)) + 10000; // 10000-99999
-    }
-}

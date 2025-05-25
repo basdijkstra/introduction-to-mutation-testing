a5507964b4982482b6634becbfe4c808373a9bef
Bas Dijkstra <bas@ontestautomation.com>
2025-05-25 08:41:07 +0200

    Add comments to tests

--- src/test/java/com/ontestautomation/mutationbank/MutationBankApplicationTests.java
+++ src/test/java/com/ontestautomation/mutationbank/MutationBankApplicationTests.java
@@ -3,9 +3,7 @@ package com.ontestautomation.mutationbank;
 import com.ontestautomation.mutationbank.clients.AccountClient;
 import com.ontestautomation.mutationbank.models.AccountDto;
 import com.ontestautomation.mutationbank.models.AccountType;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,14 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MutationBankApplicationTests {
-
-	private RequestSpecification requestSpec;
 
 	private AccountClient accountClient;
 
@@ -37,12 +30,14 @@ public class MutationBankApplicationTests {
 	@Test
 	public void createAccount() {
 
+		// Create a new checking account
 		AccountDto account = new AccountDto(AccountType.CHECKING);
-
 		int accountId = this.accountClient.createAccount(account);
 
+		// Retrieve the account to see if it has been created successfully
 		Response getAccountResponse = this.accountClient.getAccount(accountId);
 
+		// Check the response status code and the account balance (should be $0)
 		Assertions.assertEquals(200, getAccountResponse.getStatusCode());
 		Assertions.assertEquals(0.0F, (Float) getAccountResponse.path("balance"));
 	}
@@ -50,28 +45,34 @@ public class MutationBankApplicationTests {
 	@Test
 	public void getAccount() {
 
+		// GET an account that should not exist
 		Response response = this.accountClient.getAccount(1234);
 
+		// Check the response status code
 		Assertions.assertEquals(404, response.getStatusCode());
 	}
 
 	@Test
 	public void deleteAccount() {
 
+		// DELETE an account that does not exist
 		Response response = this.accountClient.deleteAccount(9876);
 
+		// DELETE should always return 204, no matter if the account existed before
 		Assertions.assertEquals(204, response.getStatusCode());
 	}
 
 	@Test
 	public void testDeposit() {
 
+		// Create a new checking account
 		AccountDto account = new AccountDto(AccountType.CHECKING);
-
 		int accountId = this.accountClient.createAccount(account);
 
+		// Deposit 10 dollars / euros / smurfs into the account
 		Response response = this.accountClient.depositToAccount(accountId, 10);
 
+		// Check that the deposit is processed correctly and that the balance is updated
 		Assertions.assertEquals(200, response.getStatusCode());
 		Assertions.assertEquals(10.0F, (Float) response.path("balance"));
 	}
@@ -79,14 +80,20 @@ public class MutationBankApplicationTests {
 	@Test
 	public void testOverdrawOnSavingsAccount() {
 
+		// Create a new savings account
 		AccountDto account = new AccountDto(AccountType.SAVINGS);
-
 		int accountId = this.accountClient.createAccount(account);
 
+		// Deposit 10 dollars / euros / smurfs into the account
 		this.accountClient.depositToAccount(accountId, 10);
 
+		// Try to withdraw 20 dollars / euros / smurfs from the account
+		// and check that this is not allowed and returns a 404
 		Response response = this.accountClient.withdrawFromAccount(accountId, 20);
-
 		Assertions.assertEquals(400, response.getStatusCode());
+
+		// Check that the account balance is unchanged
+		Response getResponse = this.accountClient.getAccount(accountId);
+		Assertions.assertEquals(10.0F, (Float) getResponse.path("balance"));
 	}
 }
